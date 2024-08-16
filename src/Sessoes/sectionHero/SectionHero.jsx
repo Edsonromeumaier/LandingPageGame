19ef4a40652c77e5182d604acee3086efae2e842
edsonromeu <romeumaier@gmail.com>
2024-08-16 16:26:31 -0300

    ajusta_header_desktop

--- src/Sessoes/sectionHero/SectionHero.jsx
+++ src/Sessoes/sectionHero/SectionHero.jsx
@@ -4,9 +4,10 @@ import Logo from '../../assets/images/Logo.svg';
 import { HiPencilAlt } from "react-icons/hi";
 import photoHero from '../../assets/images/hero-image.svg';
 import heroimage from '../../assets/images/hero-image.svg';
-import { BiStar } from 'react-icons/bi';
+import { BsStar } from 'react-icons/bs';
 import { useState } from 'react';
 import '../../Sessoes/sectionHero/SectionHero.css';
+
 
 function SectionHero(args) {
   const [isOpen, setIsOpen] = useState(false);
@@ -61,26 +62,27 @@ function SectionHero(args) {
         </ModalBody>
       </Modal>
     </Navbar>
-    <div className='px-3 px-sm-4 py-sm-5 text-white'>
-        <h1 className='fw-bold lh-base' style={{fontweight:'800'}} >Aprenda a arte de 
-        Desenvolvedor de jogos</h1>
-        <p className='mt-4 fw-small fs-5'>
-        Este é um curso abrangente sobre Desenvolvimento de Jogos. Você aprenderá tudo, desde gerar uma ideia até publicar seus jogos em diferentes plataformas.
-        </p>        
-        <Button className='rounded-5 text-white fw-bold px-4' style={{'background-color':'#da722c'}} color='#da722c'><HiPencilAlt className='mx-2'/>Inscrevase agora</Button>          
-    </div>
     
-    <div>
-        <div className='container-sm d-grid d-md-none'>   
+    <div className='container lg: row'>
+      <div className='col-lg-6 px-3 px-sm-4 py-sm-5 text-white'>
+          <h1 className='fw-bold lh-base' style={{fontweight:'800'}} >Aprenda a arte de 
+          Desenvolvedor de jogos</h1>
+          <p className='mt-4 fw-small fs-5'>
+          Este é um curso abrangente sobre Desenvolvimento de Jogos. Você aprenderá tudo, desde gerar uma ideia até publicar seus jogos em diferentes plataformas.
+          </p>        
+          <Button className='rounded-5 text-white fw-bold px-4' style={{'background-color':'#da722c'}} color='#da722c'><HiPencilAlt className='mx-2'/>Inscrevase agora</Button>          
+      </div>
+      <div className='col-lg-6'>
+        <div className='container-sm d-grid d-md-none'>            
             <div className='row'>
-                <div className='z-1 col d-grid align-items-center gap-2'>
-                    <div className='row-6 badge bg-primary text-wrap'>
-                        <p className='display-4'>30k.</p>
-                        <p className='display-7'>Alunos Matriculados</p>
+                <div className='z-1 col-4 d-grid align-items-center gap-2'>
+                    <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg' style={{'margin-right':'-3rem', 'border-right':'1em solid grey','border-bottom':'1em solid grey'}}>
+                        <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'>32k.</p>
+                        <p className='display-7 fs-6 text-black'>Alunos Matriculados</p>
                     </div>
-                    <div className='z-2 row-4 align-self-start badge bg-primary text-wrap'>
-                        <p className='display-4'> 4.7</p>
-                        <p className='display-7'>Classificação geral <BiStar/></p>
+                    <div className='z-2 row-4 align-self-start badge bg-white text-wrap shadow-lg' style={{'margin-right':'-2rem','border-right':'1em solid grey','border-bottom':'1em solid grey' }} >
+                        <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'> 4.7 <img src={BsStar} className=''  /> </p>
+                        <p className='display-7 fs-6 text-black'>Classificação geral </p>
                     </div>
                 </div>
                 <div className='col-8'>
@@ -89,17 +91,19 @@ function SectionHero(args) {
             </div>
         </div>       
         <div className='container-md d-none d-md-block d-lg-none'>
-            <div className='row'>
-                <div className='col-6'>         
-                    <div className='badge bg-primary text-wrap' style={{ width: '6rem' }}>
-                        30k magic.
+        <div className='row'>
+                <div className='z-1 col-4 d-grid align-items-center gap-2'>
+                    <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg' style={{'margin-right':'-1rem', 'border-right':'1em solid grey','border-bottom':'1em solid grey'}}>
+                        <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'>32k.</p>
+                        <p className='display-7 fs-6 text-black'>Alunos Matriculados</p>
                     </div>
-                    <div className='badge bg-primary text-wrap' style={{ width: '6rem' }}>
-                        10k magic.
+                    <div className='z-2 row-4 align-self-start badge bg-white text-wrap shadow-lg' style={{'border-right':'1em solid grey','border-bottom':'1em solid grey' }} >
+                        <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'> 4.7 <img src={BsStar} className=''  /> </p>
+                        <p className='display-7 fs-6 text-black'>Classificação geral </p>
                     </div>
                 </div>
-                <div className='col-4'>
-                    <img src={heroimage} className='rounded' alt="imagem do herói" />
+                <div className='col-8'>
+                    <img src={photoHero} className="img-fluid "  alt="imagem do herói" />
                 </div>
             </div>
         </div>
@@ -118,7 +122,9 @@ function SectionHero(args) {
                 </div>
             </div>
         </div>
+      </div>
     </div>
+    
   </div>
   );
 }
