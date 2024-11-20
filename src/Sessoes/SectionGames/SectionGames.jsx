2380083789bb33262781f427c4cb2bc71e4087c4
edsonromeu <romeumaier@gmail.com>
2024-11-20 08:54:25 -0300

    20/11

--- src/Sessoes/SectionGames/SectionGames.jsx
+++ src/Sessoes/SectionGames/SectionGames.jsx
@@ -1,49 +1,51 @@
 import marioGame from '../../assets/images/mario-game.svg'
 import game from '../../assets/images/stick-game.svg'
 import clock from '../../assets/images/clock.svg'
+
 function SectionGames () {
     return(
-<div className='wrapper'>
- <div className="container bg-warning" style={{backgroundImage: 'linear-gradient(180deg, #6f7dfb, #26308c)','font-family': 'Poppins, sans-serif' }}>
-
-  <div className='row'>
-    <div className="col text-center mb-5">
-            <h5 className='display-5 mt-4 fs-1 fw-bold lh-lg text-white'> Nossos Cursos </h5>
-    </div> 
-  </div>
-
-  <div className='row gap-3'>
-
-    <div className="card text-center" style={{'border-radius':'0.8em'}}>
-      <div className="card-body">
-        <img className="img-fluid rounded-3" src={marioGame} alt="foto dos personagens do jogos mario" style={{'margin-top':'-1em'}} />
-        <div className="d-flex justify-content-between text-start mt-4 mb-2">
-                <h5 className="fs-2 fw-bold">Fundamentos do design de jogos</h5>
-                <p className="text-center"><i className='far fa-clockl'></i> <img src={clock} alt="icone relógio" /><small> 8 mim </small></p>
-        </div>        
+<div className='wrapper' style={{
+  background:'linear-gradient( #6f7dfb, #26308c )',
+  fontFamily: 'poppins',
+}}>
+  <div className="container">  
+      <div className="row text-center mb-5 pt-5">
+              <h1 className='display-4 fs-1 fw-bold lh-lg text-white'>Nossos Cursos</h1>
       </div>
-      <div className="card-footer bg-dark rounded-pill mx-2" style={{}}>
-      </div>             
-    </div>
+    <div className='sm-row gap-3 d-md-flex flex-nowrap'>
 
-    <div className="card text-center" style={{'border-radius':'0.8em'}}>      
-      <div className="card-body" >
-        <img className="img-fluid rounded-3" src={game} alt="foto dos personagens do jogos mario" style={{'margin-top':'-1em'}}/>
-        <div className="d-flex justify-content-between text-start mt-4 mb-2">          
-          <h5 className="fs-2 fw-bold">Fundamentos do Inity Game Engine</h5>          
-          <p className=''><img src={clock} alt="icone do relógio"/>6 min</p>
+        <div className="card text-center mb-3 col-md-6" style={{'border-radius':'0.8em'}}>
+          <div className="card-body">
+            <img className="img-fluid rounded-3" src={marioGame} alt="foto dos personagens do jogos mario" style={{'margin-top':'-1em'}} />
+            <div className="d-flex justify-content-between text-start mt-4 mb-2">
+                    <h5 className="fs-2 fw-bold">Fundamentos do design de jogos</h5>
+                    <p className="text-center"><i className='far fa-clockl'></i> <img src={clock} alt="icone relógio" /><small> 8 mim </small></p>
+            </div>        
+          </div>
+          <div className="card-footer bg-dark rounded-pill mx-2" style={{}}>
+          </div>             
         </div>
-      </div>
-      <div className="card-footer bg-dark rounded-pill mx-2" style={{'margin-bottom':'-0.1rem'}} >
-      </div>
-    </div>
 
-    <div className='m-5 '>        
-          <a href="#" className="float-end text-white btn rounded-5 mx-5 px-4" style={{'background-color':'#da722c', 'font-size':'1.2em', 'fontWeight':700}}> Saiba mais </a>
-    </div>
-  </div>
-  
- </div>  
+        <div className="card text-center mb-3 col-md-6" style={{'border-radius':'0.8em'}}>      
+          <div className="card-body" >
+            <img className="img-fluid rounded-3" src={game} alt="foto dos personagens do jogos mario" style={{'margin-top':'-1em'}}/>
+            <div className="d-flex justify-content-between text-start mt-4 mb-2">          
+              <h5 className="fs-2 fw-bold">Fundamentos do Inity Game Engine</h5>          
+              <p className=''><img src={clock} alt="icone do relógio"/>6 min</p>
+            </div>
+          </div>
+          <div className="card-footer bg-dark rounded-pill mx-2" style={{'margin-bottom':'-0.1rem'}} >
+          </div>
+        </div>        
+         
+    </div>  
+    <div className="d-flex justify-content-end my-5 pb-5">
+    <a href="#" className="text-white btn rounded-5 mx-5 px-4" style={{ backgroundColor: '#da722c', fontSize: '1.2em', fontWeight: 700 }}>
+        Saiba mais2
+    </a>
+</div>
+
+  </div>  
 </div>
     );
 }
--- src/Sessoes/sectionHero/SectionHero.jsx
+++ src/Sessoes/sectionHero/SectionHero.jsx
@@ -16,16 +16,20 @@ function SectionHero(args) {
   const toggleModal = () => setIsOpen(!isOpen);
 
   return (
-    <div className='HeroBackground px-3'>
+    
+    <div className='px-3' style={{
+      background:'linear-gradient( #6f7dfb, #26308c )',
+      fontFamily: 'poppins',
+    }}>
   
     <Navbar {...args} className='pt-3'>
       <NavbarBrand href=""><img src={Logo} alt='logo image'/></NavbarBrand>
       <div className='d-none d-sm-flex justify-content-between p-2 text-white gap-3'>
         <NavLink href="#">
-          <h3 className='fs-6 fw-bold'>Sobre</h3>
+         
         </NavLink>
         <NavLink href="#">
-          <h3 className='fs-6 fw-bold'>Serviços</h3>
+          
         </NavLink>
         <NavLink href="#">
           <h3 className='fs-6 fw-bold'>Nosso trabalho</h3>
@@ -38,45 +42,45 @@ function SectionHero(args) {
             inscrevers-se Aqui!
           </a>
         </NavLink>          
+        
       </div>
       <Button 
       className='d-sm-none' 
-      color="" 
+      color="text-white" 
       onClick={toggleModal}
       >
-        <RxHamburgerMenu color='' style={{'font-size':'2rem'}}  />
+       <RxHamburgerMenu color='text-white' style={{'font-size':'2rem'}}  />
       </Button>
       <Modal isOpen={isOpen} toggle={toggleModal} className='grid text-center' fullscreen>
-        <ModalHeader className='container-fluid text-end bg-dark'></ModalHeader  >
-        <ModalBody toggle={toggleModal} className='container-fluid text-center bg-warning text-white d-flex align-items-center justify-content-center'>
+        <ModalHeader toggle={toggleModal} className='container-fluid text-end bg-dark'></ModalHeader  >
+        <ModalBody className='container-fluid text-center bg-dark text-white d-flex align-items-center justify-content-center'>
           <CardLink className='d-flex flex-column align-items-center'>
-            <NavLink href="#">
-              <h2>Sobre</h2>
-            </NavLink>
-            <NavLink href="#">
-              <h2>Serviços</h2>
-            </NavLink>
-            <NavLink href="#">
-              <h2>Nosso trabalho</h2>
-            </NavLink>
-            <Button color="secondary" className='mt-3'>Estática</Button>
+            <ul className="list-group">
+                <li className="list-group-item border-0 bg-dark" ><a className='text-decoration-none text-white' href="#!">Sobre</a></li>
+                <li className="list-group-item border-0 bg-dark" ><a className='text-decoration-none text-white' href="#!">Serviços</a></li>
+                <li className="list-group-item border-0 bg-dark" ><a className='text-decoration-none text-white' href="#!">Nosso trabalho</a></li>                
+            </ul>
+            <Button className='mt-3 rounded-5 text-white fw-bold px-4'
+           style={{'background-color':'#da722c'}}> Inscreva-se agora         
+           </Button>            
           </CardLink>
         </ModalBody>
       </Modal>
     </Navbar>
     
-    <div className=''>
-      <div className='col-lg-5 px-3 px-sm-4 py-sm-5 text-white'>
-          <h1 className='fw-bold lh-base' style={{fontweight:'800'}} >Aprenda a arte de 
-          Desenvolvedor de jogos</h1>
-          <p className='mt-4 fw-small fs-5'>
-          Este é um curso abrangente sobre Desenvolvimento de Jogos. Você aprenderá tudo, desde gerar uma ideia até publicar seus jogos em diferentes plataformas.
-          </p>        
-          <Button className='rounded-5 text-white fw-bold px-4' style={{'background-color':'#da722c'}} color='#da722c'><HiPencilAlt className='mx-2'/>Inscrevase agora</Button>          
-      </div>
-      <div className='col-lg-6'>
-        <div className='container-sm d-grid d-md-none'>            
-            <div className='row'>
+    <div className=''>      
+      <div className='col'>
+        <div className='container-sm d-grid d-md-none'>
+            <div className='col-lg-5 px-3 px-sm-4 py-sm-5 text-white'>
+              <h1 className='fw-bold lh-base' style={{fontweight:'800'}} >Aprenda a arte de 
+              Desenvolvedor de jogos</h1>
+              <p className='mt-4 fw-small fs-5'>
+              Este é um curso abrangente sobre Desenvolvimento de Jogos. Você aprenderá tudo, desde gerar uma ideia até publicar seus jogos em diferentes plataformas.
+              </p>        
+              <Button className='rounded-5 text-white fw-bold px-4' style={{'background-color':'#da722c'}} color='#da722c'><HiPencilAlt className='mx-2'/>Inscrevase agora</Button>          
+            </div>   
+            
+            <div className='row overflow-hidden'>
                 <div className='z-1 col-4 d-grid align-items-center gap-2'>
                     <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg'
                     style={{
@@ -98,42 +102,78 @@ function SectionHero(args) {
                     </div>
                 </div>
                 <div className='col-8'>
-                    <img src={photoHero} className='' style={{'margin-left':'-2em'}}  alt="imagem do herói"/>
+                    <img src={photoHero} className='' alt="imagem do herói"/>
                 </div>
             </div>
         </div>       
         <div className='container-md d-none d-md-block d-lg-none'>
-        <div className='row'>
-                <div className='z-1 col-4 d-grid align-items-center gap-2'>
-                    <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg' style={{'margin-right':'-1rem', 'border-right':'1em solid grey','border-bottom':'1em solid grey'}}>
-                        <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'>32k.</p>
-                        <p className='display-7 fs-6 text-black'>Alunos Matriculados</p>
-                    </div>
-                    <div className='z-2 row-4 align-self-start badge bg-white text-wrap shadow-lg' style={{'border-right':'1em solid grey','border-bottom':'1em solid grey' }} >
-                        <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'> 4.7 <img src={BsStar} className=''  /> </p>
-                        <p className='display-7 fs-6 text-black'>Classificação geral </p>
-                    </div>
-                </div>
-                <div className='col-8'>
-                    <img src={photoHero} className="img-fluid "  alt="imagem do herói" />
-                </div>
-            </div>
-        </div>
-        <div className="container-lg d-none d-lg-block">            
-        <div className="row">
+          <div className='col-lg-5 px-3 px-sm-4 py-sm-5 text-white'>
+                <h1 className='fw-bold lh-base' style={{fontweight:'800'}} >Aprenda a arte de 
+                Desenvolvedor de jogos</h1>
+                <p className='mt-4 fw-small fs-5'>
+                Este é um curso abrangente sobre Desenvolvimento de Jogos. Você aprenderá tudo, desde gerar uma ideia até publicar seus jogos em diferentes plataformas.
+                </p>        
+                <Button className='rounded-5 text-white fw-bold px-4' style={{'background-color':'#da722c'}} color='#da722c'><HiPencilAlt className='mx-2'/>Inscrevase agora</Button>          
+          </div>   
+          <div className='row'>
+                  <div className='z-1 col-4 d-grid align-items-center gap-2'>
+                      <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg'
+                       style={{
+                        'margin-right':'-3rem',                      
+                        'border-radius':'1.8rem',
+                        'border-right':'1em solid #3B448F',
+                        'border-bottom':'1em solid #6F7DFB'}}>
+                          <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'>32k.</p>
+                          <p className='display-7 fs-6 text-black'>Alunos Matriculados</p>
+                      </div>
+                      <div className='z-2 row-4 align-self-start badge bg-white text-wrap shadow-lg'
+                      style={{
+                        'margin-right':'-1.5rem',                      
+                        'border-radius':'1.8rem',
+                        'border-right':'1em solid #3B448F',
+                        'border-bottom':'1em solid #6F7DFB'}}>
+                          <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'> 4.7 <img src={BsStar} className=''  /> </p>
+                          <p className='display-7 fs-6 text-black'>Classificação geral </p>
+                      </div>
+                  </div>
+                  <div className='col-8'>
+                      <img src={photoHero} className="img-fluid "  alt="imagem do herói" />
+                  </div>
+              </div>
+          </div>
+        <div className="container-lg d-none d-lg-flex mx-auto">
+            <div className='col-5 px-3 px-sm-4 py-sm-5 text-white'>
+              <h1 className='fw-bold lh-base' style={{fontweight:'800'}} >Aprenda a arte de 
+              Desenvolvedor de jogos</h1>
+              <p className='mt-4 fw-small fs-5'>
+              Este é um curso abrangente sobre Desenvolvimento de Jogos. Você aprenderá tudo, desde gerar uma ideia até publicar seus jogos em diferentes plataformas.
+              </p>        
+              <Button className='rounded-5 text-white fw-bold px-4' style={{'background-color':'#da722c'}} color='#da722c'><HiPencilAlt className='mx-2'/>Inscrevase agora</Button>          
+            </div>                
+            <div className="row">
             <div className='z-1 col-4 d-grid align-items-center gap-2'>
-                    <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg' style={{'margin-right':'-1rem', 'border-right':'1em solid grey','border-bottom':'1em solid grey'}}>
+                    <div className='row-6 badge bg-white text-wrap shadow-lg rounded-lg'
+                    style={{
+                      'margin-right':'-1rem',                      
+                      'border-radius':'1.8rem',
+                      'border-right':'1em solid #3B448F',
+                      'border-bottom':'1em solid #6F7DFB'}}>
                         <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'>32k.</p>
                         <p className='display-7 fs-6 text-black'>Alunos Matriculados</p>
                     </div>
-                    <div className='z-2 row-4 align-self-start badge bg-white text-wrap shadow-lg' style={{'border-right':'1em solid grey','border-bottom':'1em solid grey' }} >
+                    <div className='z-2 row-4 align-self-start badge bg-white text-wrap shadow-lg'
+                    style={{
+                      'border-radius':'1.8rem',
+                      'margin-right':'-1rem',                      
+                      'border-right':'1em solid #3B448F',
+                      'border-bottom':'1em solid #6F7DFB' }} >
                         <p className='display-4 fs-1 fw-bold text-black mt-1 mb-1'> 4.7 <img src={BsStar} className=''  /> </p>
                         <p className='display-7 fs-6 text-black'>Classificação geral </p>
                     </div>
-                </div>
-                <div className="col-8 text-end">
-                    <img src={heroimage} className="rounded"  alt="imagem do herói" />
-                </div>
+            </div>            
+            <div className="col-lg-8">
+              <img src={heroimage} className="rounded"  alt="imagem do herói" />
+            </div>
             </div>
         </div>
       </div>
